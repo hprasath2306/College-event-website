3313d9327fefd0b037dbfdeff3e6401ee82b5910
hprasath2306 <hprasathoff935@gmail.com>
2025-03-18 12:14:06 +0530

    Registration closed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Events from "./pages/Events";
 import Header from "./components/Header";
 import Team from "./pages/Team";
 import Footer from "./components/Footer";
